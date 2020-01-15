87f8d1779e47ea6c6ef689b95228e05fb9dc62f4
Chuck5ta <chuckstarrrr@googlemail.com>
2020-01-15 12:36:49 +0000

    Implemented the creation of a pyramid

--- Assets/CreateTriangles.cs
+++ Assets/CreateTriangles.cs
@@ -2,37 +2,25 @@
 
 public class CreateTriangles : MonoBehaviour
 {
-    public Material triangleMaterial;
-
-    void CreateTriangle(Vector3 voxel0, Vector3 voxel1, Vector3 voxel2)
+    void CreateTriangle(Vector3 voxel0, Vector3 voxel1, Vector3 voxel2, Color meshColour)
     {
         print("Creating triangle");
         Mesh mesh = new Mesh();
-        mesh.name = "Triangle";
+        mesh.name = "Triangle"; // MAKE THIS UNIQUE!!!
 
-        Vector3[] vertices = new Vector3[3]; // total number of vertices of object created (cube = 8, quad = 4, triangle = 3)
-        Vector3[] normals = new Vector3[3];
-        Vector2[] uvs = new Vector2[3];
-        int[] triangles = new int[3]; //the number of vertices making up a triangle
+        // define the actual shape/primitive - 3 vertices = triangle, 4 vertices = square/quad
+        Vector3[] newVertices = new Vector3[] { voxel0, voxel1, voxel2 };
 
         //all possible UVs
         Vector2 uv00 = new Vector2(0f, 0f);
-        Vector2 uv10 = new Vector2(1f, 0f);
         Vector2 uv01 = new Vector2(0f, 1f);
+        Vector2 uv11 = new Vector2(1f, 1f);
+        Vector2[] newUV = new Vector2[] { uv11, uv01, uv00 }; // # of UVs
+        int[] newTriangles = new int[] { 0, 1, 2 }; // # of vertices - triangle = 3, quad = 6 (made up of 2 triangles)
 
-        // define the actual shape/primitive - 3 vertices = triangle, 4 vertices = square/quad
-        vertices = new Vector3[] { voxel0, voxel1, voxel2 };
-        normals = new Vector3[] {Vector3.forward, // direction in which the face can be seen (other side will be invisible)
-                                 Vector3.forward,
-                                 Vector3.forward};
-
-        uvs = new Vector2[] { uv00, uv10, uv01 }; // # of UVs
-        triangles = new int[] { 0, 1, 2 }; // # of vertices
-
-        mesh.vertices = vertices;
-        mesh.normals = normals;
-        mesh.uv = uvs;
-        mesh.triangles = triangles;
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
 
         mesh.RecalculateBounds();
 
@@ -42,13 +30,10 @@ public class CreateTriangles : MonoBehaviour
         meshFilter.mesh = mesh;
         // display the triangle
         MeshRenderer renderer = triangle.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
-        // set the triangle's material
-        renderer.material = triangleMaterial;
-
+        renderer.material.color = meshColour;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void CreatePyramid()
     {
         // X Y Z axis
         // triangle voxel 1
@@ -57,7 +42,38 @@ public class CreateTriangles : MonoBehaviour
         Vector3 voxel1 = new Vector3(0.5f, 0f, 0.5f);
         // triangle voxel 3
         Vector3 voxel2 = new Vector3(0.5f, 0f, -0.5f);
-        CreateTriangle(voxel0, voxel1, voxel2);
+        CreateTriangle(voxel0, voxel1, voxel2, Color.blue); // up - order of the coords = direction of the normals
+                                                            // up facing face - facing up towards positive Y axis
+        // triangle voxel 1
+        voxel0 = new Vector3(-0.5f, 0f, 0.5f);
+        // triangle voxel 2
+        voxel1 = new Vector3(0.5f, 0f, 0.5f);
+        // triangle voxel 3
+        voxel2 = new Vector3(0.5f, -1f, -1f);
+        CreateTriangle(voxel2, voxel1, voxel0, Color.red); // forwards/front facing face - facing towards positive Z axis
+
+        // triangle voxel 1
+        voxel0 = new Vector3(-0.5f, 0f, 0.5f);
+        // triangle voxel 2
+        voxel1 = new Vector3(0.5f, -1f, -1f);
+        // triangle voxel 3
+        voxel2 = new Vector3(0.5f, 0f, -0.5f);
+        CreateTriangle(voxel1, voxel0, voxel2, Color.cyan); // back facing face - facing towards negative Z axis
+
+        // triangle voxel 1
+        voxel0 = new Vector3(0.5f, -1f, -1f);
+        // triangle voxel 2
+        voxel1 = new Vector3(0.5f, 0f, 0.5f);
+        // triangle voxel 3
+        voxel2 = new Vector3(0.5f, 0f, -0.5f);
+        CreateTriangle(voxel2, voxel1, voxel0, Color.green); // right facing face - facing towards positive X axis
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {     
+        CreatePyramid();
+        //  CreateTriangle();
     }
 
     // Update is called once per frame
