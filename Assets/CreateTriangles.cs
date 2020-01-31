b920c9e74799e71e6e27f1287fa5c506c96f6362
Chuck5ta <chuckstarrrr@googlemail.com>
2020-01-31 16:14:23 +0000

    Changing to terrain made out of quads, not pyramids or cubes

--- Assets/CreateTriangles.cs
+++ Assets/CreateTriangles.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 
+/*
+ * 31 JAN 2020
+ * Build the terrain out of quads, each made out of 2 triangles
+ *  
+ * I don't think this needs cubes or any 3D objects for the terrain. You just alter the vertex coordinates and rerender?
+ * May need to add remove geometry (triangles or quads)
+ * 
+ */
+
 public class CreateTriangles : MonoBehaviour
 {
     private float Xaxis = 0f; // defaulted to the centre of the world
     private float Yaxis = 0f; // defaulted to the centre of the world
     private float Zaxis = 0f; // defaulted to the centre of the world
 
-    void CreateTriangle(Vector3 vertex0, Vector3 vertex1, Vector3 vertex2, Color meshColour)
+    
+    void CreateTriangle(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Color meshColour)
     {
         print("Creating triangle");
         Mesh mesh = new Mesh();
         mesh.name = "Triangle"; // MAKE THIS UNIQUE!!!
 
         // define the actual shape/primitive - 3 vertices = triangle, 4 vertices = square/quad
-        Vector3[] newVertices = new Vector3[] { vertex0, vertex1, vertex2 };
+        Vector3[] newVertices = new Vector3[] { vertex1, vertex2, vertex3 };
 
         //all possible UVs
         Vector2 uv00 = new Vector2(0f, 0f);
@@ -26,7 +36,7 @@ public class CreateTriangles : MonoBehaviour
         mesh.uv = newUV;
         mesh.triangles = newTriangles;
 
-        mesh.RecalculateBounds();
+        mesh.RecalculateBounds(); // this is used for rendering
 
         GameObject triangle = new GameObject("triangle");
         triangle.transform.parent = this.gameObject.transform;
@@ -36,7 +46,32 @@ public class CreateTriangles : MonoBehaviour
         MeshRenderer renderer = triangle.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
         renderer.material.color = meshColour;
     }
+    
+    // A quad is made out of 2 triangles
+    void CreateQuad(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Color meshColour)
+    {
+        print("Creating quad");
+        Mesh mesh = new Mesh();
+        mesh.name = "Quad"; // MAKE THIS UNIQUE!!!
 
+        // create 1st triangle
+        CreateTriangle(vertex1, vertex2, vertex3, Color.blue);
+
+        // create 2nd triangle
+        Vector3 triangleTwoVertex1 = new Vector3(Xaxis, Yaxis, Zaxis + 0.5f);
+        Vector3 triangleTwoVertex2 = new Vector3(Xaxis + 0.5f, Yaxis, Zaxis + 0.5f);
+        Vector3 triangleTwoVertex3 = new Vector3(Xaxis + 0.5f, Yaxis, Zaxis);
+        CreateTriangle(triangleTwoVertex1, triangleTwoVertex2, triangleTwoVertex3, Color.red);
+
+        // do we need to combine the traingles into one mesh? 
+        // possibly
+        // we can still alter the coordinates of all the vertices of the triangles that make up the quad
+        // so, yes we should combine the triangles into a quad mesh
+
+        // COMBINE HERE
+    }
+
+    /* INEFFICIENT - far better to use quads - far less geometry required
     void CreatePyramid()
     {
         // X Y Z axis
@@ -72,12 +107,17 @@ public class CreateTriangles : MonoBehaviour
         vertex2 = new Vector3(Xaxis + 0.5f, Yaxis, Zaxis - 0.5f);
         CreateTriangle(vertex2, vertex1, vertex0, Color.green); // right facing face - facing towards positive X axis
     }
+    */
 
     // Start is called before the first frame update
     void Start()
-    {     
-        CreatePyramid();
-        //  CreateTriangle();
+    {
+        // Initial triangle vertices coordinates
+        Vector3 vertex1 = new Vector3(Xaxis, Yaxis, Zaxis);
+        Vector3 vertex2 = new Vector3(Xaxis, Yaxis, Zaxis + 0.5f);
+        Vector3 vertex3 = new Vector3(Xaxis + 0.5f, Yaxis, Zaxis);
+
+        CreateQuad(vertex1, vertex2, vertex3, Color.blue);
     }
 
     // Update is called once per frame
